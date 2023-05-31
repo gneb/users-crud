d80bcf900e72b9dfb2c3623081e27c1887acab37
gneb <gneb@outlook.com>
2023-05-31 19:28:43 +0400

    finished layout and views structure

---
+++ resources/partials/public/styles.blade.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+{{ URL::asset('css/public.css'); }} 
---
+++ resources/views/public/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.public.master')
+
+@section('content')
+    <div class="card mx-auto">
+    <div class="card-body">
+        <h5 class="card-title">Users crud</h5>
+        <p class="card-text">We have 25 users in our database</p>
+        <a href="#" class="btn btn-primary">Login to admin</a>
+    </div>
+    </div>
+@endsection
