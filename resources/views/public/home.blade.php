f626a61cffeef1c27b06a4ba291cacbb848466c2
gneb <gneb@outlook.com>
2023-06-02 00:40:39 +0400

    updated home template

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -3,24 +3,10 @@
     <div class="card-body">
         <h5 class="card-title">Users crud</h5>
         <p class="card-text">
-            @guest We have 25 users in our database.
+            @guest We have {{ $usersCount }} users in our database.
             <a href="{{ url('/login') }}" title="login">Login</a>
-            @endguest @auth 
-            <ol class="list-group list-group-numbered">
-                <li class="list-group-item border-0">
-                    First name: <b>{{Auth::user()->first_name}}</b>
-                </li>
-                <li class="list-group-item border-0">
-                    Last name: <b>{{Auth::user()->last_name}}</b>
-                </li>
-                <li class="list-group-item border-0">
-                    Username: <b>{{Auth::user()->username}}</b>
-                </li>
-                <li class="list-group-item border-0">
-                    Age: <b>{{Auth::user()->age}}</b>
-                </li>
-            </ol>
-            @endauth
+            @endguest @auth @include('partials.public.user_card', ['user' =>
+            Auth()->user()]) @endauth
         </p>
     </div>
 </div>
