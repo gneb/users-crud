c8328b64ef548763d395872286245b823a8f793d
gneb <gneb@outlook.com>
2023-06-02 00:40:10 +0400

    added users count

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\View\View;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class PublicController extends Controller
 {
     public function show(): View
     {
-        return view('public.home');
+        return view('public.home', ['usersCount' => User::all()->count()]);
     }
 }
 
